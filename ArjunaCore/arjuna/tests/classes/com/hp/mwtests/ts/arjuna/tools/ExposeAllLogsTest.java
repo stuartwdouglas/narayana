ceb0914fff768042e5a6684c90182532e8b0693c
Jesper Pedersen <jesper.pedersen@redhat.com>
2014-06-24 15:04:37 -0400

    [JBTM-2203] Make default name null in order for quick lookup
    Conflicts:
    	ArjunaCore/arjuna/tests/classes/com/hp/mwtests/ts/arjuna/tools/ExposeAllLogsTest.java

--- ArjunaCore/arjuna/tests/classes/com/hp/mwtests/ts/arjuna/tools/ExposeAllLogsTest.java
+++ ArjunaCore/arjuna/tests/classes/com/hp/mwtests/ts/arjuna/tools/ExposeAllLogsTest.java
@@ -124,6 +124,7 @@ public class ExposeAllLogsTest {
     }
 
     private void probeObjectStore(boolean exposeAllLogs, boolean useJMX) {
+
         ObjectStoreEnvironmentBean osEnvBean = BeanPopulator.getDefaultInstance(ObjectStoreEnvironmentBean.class);
 
         osEnvBean.setExposeAllLogRecordsAsMBeans(exposeAllLogs);
